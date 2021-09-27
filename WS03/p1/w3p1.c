6aaae44a5e8d02ef31a78964c1380a6d60989330
Zander <28664144+ZandercraftGames@users.noreply.github.com>
2021-09-27 16:00:58 -0400

    Prepare Workshop 3

--- WS03/p1/w3p1.c
+++ WS03/p1/w3p1.c
@@ -2,10 +2,38 @@
 	==================================================
 	Workshop #3 (Part-1):
 	==================================================
-	Name   :
-	ID     :
-	Email  :
-	Section:
+	Name   : Zander Maitland
+	ID     : 112303219
+	Email  : zmaitland@myseneca.ca
+	Section: NCC
 */
-#define _CRT_SECURE_NO_WARNINGS
 
+// Imports
+#include <stdio.h>  // Standard IO
+#include <stdlib.h> // Standard Library Functions
+
+// Macro Definitions
+#define _CRT_SECURE_NO_WARNINGS
+#define MIN_YEAR 2010  // Minimum year that can be entered.
+#define MAX_YEAR 2021  // Maximum year that can be entered.
+
+// Main Function
+int main(void)
+{
+    /*
+     * Steps:
+     * 1. Display title for application
+     * 2. Prompt the user
+     * 3.
+     */
+
+    // Main Variables
+    const int JAN = 1;  // Assign constants for January and December's mo. nums.
+    const int DEC = 12;
+
+    // Application Introduction
+    printf("General Well-bring Log\n"
+           "======================\n");
+
+    return 0;
+}
--- WS03/p2/w3p2.c
+++ WS03/p2/w3p2.c
@@ -2,10 +2,10 @@
 	==================================================
 	Workshop #3 (Part-2):
 	==================================================
-	Name   :
-	ID     :
-	Email  :
-	Section:
+	Name   : Zander Maitland
+	ID     : 112303219
+	Email  : zmaitland@myseneca.ca
+	Section: NCC
 */
 #define _CRT_SECURE_NO_WARNINGS
 
