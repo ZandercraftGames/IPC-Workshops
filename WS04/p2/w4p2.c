df0ae9c3d6eee215157dbd5becf6c17174ef58c6
Zander <28664144+ZandercraftGames@users.noreply.github.com>
2021-10-09 18:43:25 -0400

    Workshop 4 Part 2 Final 1.

--- WS04/p2/w4p2.c
+++ WS04/p2/w4p2.c
@@ -22,7 +22,13 @@ int main(void) {
     double userIncome;                // The monthly net income that the user provides.
     int itemCount;                    // The number of wish list items that the user provides.
     const double min_cost = 100;      // The minimum cost of an item.
-    double totalCost = 0;                 // Keeps track of the total cost of all items added together.
+    double totalCost = 0;             // Keeps track of the total cost of all items added together.
+    int forecastSelection;            // Keeps track of the forecast type that the user has selected.
+    int hasFinance;                   // Keeps track of if any item has finance options.
+    int forecastMonths;               // Keep track of the number of months it will take to pay off the items.
+    int priorityFilter;               // Keeps track of the priority filter input when selecting to filter by priority.
+    double priorityFilterCost = 0;    // Keeps track of the cost of items when run through the priority filter.
+    int priorityFilterHasFinance;     // Keep track of if the priority filter has finance options.
 
     // Parallel Array Declaration
     double itemCost[MAX_ITEMS];       // The array used to store the cost of the items.
@@ -78,6 +84,7 @@ int main(void) {
 
     int i; // Initialize the variable before the loop for backwards compatibility with older C versions.
     for (i = 1; i <= itemCount; i++) {
+        correct = 0; // Reset validity tracker for use in next loop iteration
         // Print introduction to item
         printf("\n"
                "Item-%d Details:\n", i);
@@ -147,16 +154,112 @@ int main(void) {
     int j;  // Declare before loop for compatibility
     for (j = 1; j <= itemCount; j++) {
         printf("%3d  %5d    %5c    %11.2lf\n", j, itemPriority[j - 1], itemFinance[j - 1], itemCost[j - 1]);
+        if (itemFinance[j - 1] == 'y') {
+            hasFinance = 1;
+        }
     }
 
     // Print summary to user
     printf("---- -------- -------- -----------\n"
            "                      $%11.2lf\n", totalCost);
 
-    // Begin input validation loop for forecast menu
+    // Start input validation loop for forecast menu
     while (!correct) {
-        correct = 1;
+        // Prompt user for input
+        printf("\n"
+               "How do you want to forecast your wish list?\n"
+               " 1. All items (no filter)\n"
+               " 2. By priority\n"
+               " 0. Quit/Exit\n"
+               "Selection: ");
+        scanf("%d", &forecastSelection);
+
+        // Validate user's input
+        if (forecastSelection == 1) {
+            // User wants all values.
+            correct = 0;
+
+            printf("\n====================================================\n");
+
+            // Calculations
+            forecastMonths = ((int) ((totalCost * 100) + 0.5) / (int) ((userIncome * 100) + 0.5)) + 1;  // Calculate based on income and cost in cents. 0.5 added to prevent rounding errors and 1 for the end of the month.
+
+            // Output details
+            printf("Filter:   All items\n"
+                   "Amount:   $%.2lf\n"
+                   "Forecast: %d years, %d months\n", totalCost, forecastMonths / 12, forecastMonths % 12);
+
+            if (hasFinance) {
+                printf("NOTE: Financing options are available on some items.\n"
+                       "      You can likely reduce the estimated months.\n");
+            }
+
+            printf("====================================================\n");
+
+        } else if (forecastSelection == 2) {
+            // User wants to filter by priority.
+            correct = 0;
+
+            // Begin nested validation loop
+            while (!correct) {
+                // Prompt user to provide priority to filter by.
+                printf("\n"
+                       "What priority do you want to filter by? [1-3]: ");
+                scanf("%d", &priorityFilter);
+
+                // Validate response
+                if ((priorityFilter >= 1) && (priorityFilter <= 3)) {
+                    correct = 1;
+                } else {
+                    // Wrong input. Return error.
+                    printf("ERROR: Value must be between 1 and 3\n");
+                    correct = 0;
+                }
+            }
+
+            // Iterate through all entries searching for those with the right priority
+            int k;  // Defined outside of loop for backwards compatibility with previous versions of C.
+            for (k = 1; k <= MAX_ITEMS; k++)
+            {
+                if (itemPriority[k - 1] == priorityFilter) {
+                    priorityFilterCost += itemCost[k - 1];  // Add to the filtered cost.
+                    if (itemFinance[k - 1] == 'y') {
+                        priorityFilterHasFinance = 1;
+                    }
+                }
+            }
+
+            correct = 0;  // Reset for next loop iteration
+
+            // Calculations
+            forecastMonths = ((int) ((priorityFilterCost * 100) + 0.5) / (int) ((userIncome * 100) + 0.5)) + 1;  // Calculate based on income and cost in cents. 0.5 added to prevent rounding errors and add one since it represents the whole month.
+
+            printf("\n====================================================\n");
+
+            printf("Filter:   by priority (%d)\n"
+                   "Amount:   $%.2lf\n"
+                   "Forecast: %d years, %d months\n", priorityFilter, priorityFilterCost, forecastMonths / 12, forecastMonths % 12);
+
+            if (priorityFilterHasFinance) {
+                printf("NOTE: Financing options are available on some items.\n"
+                       "      You can likely reduce the estimated months.\n");
+            }
+
+            printf("====================================================\n");
+        } else if (forecastSelection == 0) {
+            // Ends loop and exits the program.
+            correct = 1;
+        } else {
+            // The user input an invalid value. Return error.
+            printf("\n"
+                   "ERROR: Invalid menu selection.\n");
+            correct = 0;
+        }
+        priorityFilterCost = 0;  // Reset cost so the program doesn't double it on th next run.
+        priorityFilterHasFinance = 0; // Reset status for next run.
     }
+
+    correct = 0; // Reset validity tracker for use in next loop.
 
     // Print exit message
     printf("\n"
